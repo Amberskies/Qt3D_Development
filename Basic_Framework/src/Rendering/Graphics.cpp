96c113cf5d6ae55831f11b80ab0803445a9b2f75
Frazor 'Blades' Sharp <daocamberskies@googlemail.com>
2018-06-08 15:45:20 +0100

    Revert "Added Textures to Graphics::test()"
    This reverts commit 386cd9e28f2990b8099bb6495007ed2c5d772473.

--- Basic_Framework/src/Rendering/Graphics.cpp
+++ Basic_Framework/src/Rendering/Graphics.cpp
@@ -26,35 +26,18 @@ void Graphics::EndFrame()
 	// Empty
 }
 
-#include <QTexture>
-#include <QTextureImage>
-#include <QTextureMaterial>
-
-
 void Graphics::test()
 {
-	m_entity = new Qt3DCore::QEntity(m_rootEntity);
-
 	Qt3DRender::QMesh *testMesh = new Qt3DRender::QMesh();
-	testMesh->setSource(QUrl::fromLocalFile("Assets/Mesh/Creeper.ply"));
+	testMesh->setSource(QUrl::fromLocalFile("Assets/Mesh/Dice.ply"));
 	qWarning("test mesh loading");
-
-	Qt3DRender::QTexture2D *texture = new Qt3DRender::QTexture2D();
-
-	Qt3DRender::QTextureImage *tex = new Qt3DRender::QTextureImage();
-	tex->setSource(QUrl::fromLocalFile("Assets/Mesh/creeper.png"));
-
-	texture->addTextureImage(tex);
-	
-	Qt3DExtras::QTextureMaterial *testMaterial = new Qt3DExtras::QTextureMaterial();
-	testMaterial->setTexture(texture);
-	//Qt3DExtras::QPhongMaterial *testMaterial = new Qt3DExtras::QPhongMaterial();
-	//testMaterial->setDiffuse(QColor(QRgb(0xD4AF37)));
+	Qt3DExtras::QPhongMaterial *testMaterial = new Qt3DExtras::QPhongMaterial();
+	testMaterial->setDiffuse(QColor(QRgb(0xD4AF37)));
 
 	Qt3DCore::QTransform *testTransform = new Qt3DCore::QTransform();
-	testTransform->setTranslation(QVector3D(0.0f, 0.0f, -5.0f));
+	testTransform->setTranslation(QVector3D(15.0f, 1.0f, 15.0f));
 
-	
+	m_entity = new Qt3DCore::QEntity(m_rootEntity);
 	m_entity->addComponent(testMesh);
 	m_entity->addComponent(testMaterial);
 	m_entity->addComponent(testTransform);
